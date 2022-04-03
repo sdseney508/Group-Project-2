8224d7d5698f8e6d9d1d3f0246a18020095fa6fa
Steve "Homeschool" Seney <sdseney508@gmail.com>
2022-04-03 10:50:10 -0400

    single pokemon search complete.  still need to work on network efficiency and a pull of 10 random pokemon for making cards.

--- fetchpv2.js
+++ fetchpv2.js
@@ -1,101 +1,105 @@
 Pokemon = require('pokemon.js');
-const fs = require('fs');
 Pokemon.setLanguage('english');
-
-
 const axios = require('axios');
-let poke_id = 1;
+const { response } = require('express');
 
+let poke_id = 15;
+let id = 15;
+const irl = 3;
 const url = `https://pokeapi.co/api/v2/pokemon/${poke_id}`;
+const all_url = `https://pokeapi.co/api/v2/pokemon/`;
+let gen_url = 'https://pokeapi.co/api/v2/pokemon-species/';
 //initialize global variable
-let name = '';
-let evolutions = [];
-let generation = [];
-let types = [];
-let abilities = [];
-let strengths = [];
-let weaknesses = [];
-let games = [];
-let weight = '';
-let base_hp = '';
-let base_attack = '';
-let base_defense = '';
-let speed = '';
-let image_url = '';
-let evo_pic = [];
+let pokeobjects = [];
+// const findversion = (versions) => {
+//     for (const property in response.data.sprites.versions) {
+//         for (const genProp in versions[property]) {
+//             if (versions[property][genProp])
+//         };
 
+//     }
+// }
 
-axios.get(url).then(async function (response) {
-    name = response.data.name;
-    weight = response.data.weight;
-    base_hp = response.data.stats[0].base_stat;
-    base_attack = response.data.stats[1].base_stat;
-    base_defense = response.data.stats[2].base_stat;
-    speed = response.data.stats[5].base_stat;
-    image_url = response.data.sprites.front_default;
+const init = async () => {
+    // await axios.get(all_url).then(async function (response) {
 
-    let evo_loop = await Pokemon.getEvolutionLine(name).then(function (response) {
-        // console.log(response);
-        for (i = 0; i < response.length; i++) {
-            evolutions[i] = response[i];
-        };
-    });
+    //     for (irl = 148; irl < 155; irl++) {
+            let name = '';
+            let evolutions = [];
+            let generation = [];
+            let types = [];
+            let abilities = [];
+            let strengths = [];
+            let weaknesses = [];
+            let weight = '';
+            let base_hp = '';
+            let base_attack = '';
+            let base_defense = '';
+            let speed = '';
+            let image_url = '';
+            let evo_pic = [];
+            
+            let newurl = 'https://pokeapi.co/api/v2/pokemon/' + irl;
+            
+            // g_url = gen_url + irl;
+            // await axios.get(g_url).then(function (response) {
+            //     generation = response.data.generation.name;
+            // });
 
-    for (i = 0; i < evolutions.length; i++) {
-        evo_url = `https://pokeapi.co/api/v2/pokemon/${evolutions[i]}`
-        await axios.get(evo_url).then(function (response) {
-            evo_pic.push(response.data.sprites.front_default);
-        });
-    };
+            await axios.get(newurl).then(async function (response) {
+                name = response.data.name;
+                weight = response.data.weight;
+                base_hp = response.data.stats[0].base_stat;
+                base_attack = response.data.stats[1].base_stat;
+                base_defense = response.data.stats[2].base_stat;
+                speed = response.data.stats[5].base_stat;
+                image_url = response.data.sprites.front_default;
+             
+                types = response.data.types.map(({type}) => type.name);
+                abilities = response.data.abilities.map(({ability}) => ability.name)
+                // for (let i = 0; i < (response.data.types).length; i++) {
+                //     types[i] = response.data.types[i].type.name;
+                // };
 
-    for (i = 0; i < (response.data.types).length; i++) {
-        types[i] = response.data.types[i].type.name;
-    };
+                // for (let i = 0; i < (response.data.abilities).length; i++) {
+                //     abilities[i] = response.data.abilities[i].ability.name;
+                // };
 
-    for (i = 0; i < (response.data.abilities).length; i++) {
-        abilities[i] = response.data.abilities[i].ability.name;
-    };
+                //sequence through the types and create an arrayof types they are strong and weak against
+                for (i = 0; i < (types).length; i++) {
+                    //first strengthes then weaknesses
+                    //get the type of pokemon
+                    let ptype = types[i];
+    
+                    // pass that to the api via a string literal
+                    let type_url = `https://pokeapi.co/api/v2/type/${ptype}`
+    
+                    //make the axios call
+                    await axios.get(type_url).then(function (response) {
+                        // get strengths by looping through the double damage to array
+                        for (let i = 0; i < response.data.damage_relations.double_damage_to.length; i++) {
+                            strengths[i] = response.data.damage_relations.double_damage_to[i].name;
+                            weaknesses[i] = response.data.damage_relations.double_damage_from[i].name;
+                        };
+                    });
+                };
+            });
 
-    //sequence through the types and create an arrayof types they are strong and weak against
-    for (i = 0; i < (types).length; i++) {
-        //first strengthes then weaknesses
-        //get the type of pokemon
-        let ptype = types[i];
+            await Pokemon.getEvolutionLine(irl).then(async function (response) {
+                for (let ev = 0; ev < response.length; ev++) {
+                    evolutions[ev] = response[ev];
+                    evo_pic.push(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${evolutions[ev]}.png`);
+                };
+            });
 
-        // pass that to the api via a string literal
-        let type_url = `https://pokeapi.co/api/v2/type/${ptype}`
+            poke_object = { irl, name, types, weight, abilities, weaknesses, strengths, base_attack, base_defense, speed, evolutions, evo_pic, generation };
+            
+            console.log(newurl);
+            // console.log(evo_pic);
+            pokeobjects.push(poke_object);
+        // };
+    // });
+    console.log(pokeobjects);
+};
 
-        //make the axios call
-        await axios.get(type_url).then(function (response) {
-            // console.log(response.data.damage_relations);
-            // get strengths by looping through the double damage to array
-            for (i = 0; i < response.data.damage_relations.double_damage_to.length; i++) {
-                strengths[i] = response.data.damage_relations.double_damage_to[i].name;
-            };
-
-            //now weaknesses
-            for (i = 0; i < response.data.damage_relations.double_damage_from.length; i++) {
-                weaknesses[i] = response.data.damage_relations.double_damage_from[i].name;
-            };
-        });
-    };
-
-    for (i = 0; i < (response.data.game_indices).length; i++) {
-        games[i] = response.data.game_indices.name;
-    };
-
-    // console.log(response);
-    console.log('Name: ' + name + "\nType: " + types);
-    console.log('Weight: ' + weight);
-    console.log('Image Url: ' + image_url);
-    console.log('Abilities: ' + abilities)
-    console.log('Strong against: ' + strengths);
-    console.log('Weak against: ' + weaknesses);
-    console.log('Base Attack: ' + base_attack);
-    console.log('Base Defense: ' + base_defense);
-    console.log('Speed: ' + speed);
-    console.log('Evolution Line: ');
-    console.log(evolutions);
-    console.log(evo_pic);
-
-});
+init();
--- utils/pokehelper.js
+++ utils/pokehelper.js
@@ -4,7 +4,8 @@ Pokemon.setLanguage('english');
 
 const axios = require('axios');
 
-let url = `https://pokeapi.co/api/v2/pokemon/`;
+let p_url = `https://pokeapi.co/api/v2/pokemon/`;
+let gen_url = 'https://pokeapi.co/api/v2/pokemon-species/';
 //initialize global variable
 let evolutions = [];
 let gen = [];
@@ -18,19 +19,21 @@ let base_hp = '';
 let base_attack = '';
 let base_defense = '';
 let speed = '';
-
+let image_url = '';
+let evo_pic = [];
 
 const get_one = async (id) => {
-    const p_url = url + id;
+
+    p_url = url + id;
     await axios.get(p_url).then(async function (response) {
+        name = response.data.name;
         weight = response.data.weight;
         base_hp = response.data.stats[0].base_stat;
         base_attack = response.data.stats[1].base_stat;
         base_defense = response.data.stats[2].base_stat;
         speed = response.data.stats[5].base_stat;
-
-        // let generation = Pokemon.getGeneration(1);
-
+        image_url = response.data.sprites.front_default;
+   
         for (i = 0; i < (response.data.types).length; i++) {
             types[i] = response.data.types[i].type.name;
         };
@@ -45,12 +48,11 @@ const get_one = async (id) => {
             //get the type of pokemon
             let ptype = types[i];
 
-            // pass that to the api via a string literal
+            // pass that to the api via a string literal, this is a different api lpocation than the 
+            //overall pokemon one
             let type_url = `https://pokeapi.co/api/v2/type/${ptype}`
 
-            //make the axios call
             await axios.get(type_url).then(function (response) {
-                // console.log(response.data.damage_relations);
                 // get strengths by looping through the double damage to array
                 for (i = 0; i < response.data.damage_relations.double_damage_to.length; i++) {
                     strengths[i] = response.data.damage_relations.double_damage_to[i].name;
@@ -62,36 +64,30 @@ const get_one = async (id) => {
                     weaknesses[i] = response.data.damage_relations.double_damage_from[i].name;
                 };
             });
+        };        
+    });
+
+    await Pokemon.getEvolutionLine(name).then(async function (response) {
+        for (i = 0; i < response.length; i++) {
+            evolutions[i] = response[i];
+            evo_url = url + evolutions[i];
+            await axios.get(evo_url).then(function (response) {
+                evo_pic.push(response.data.sprites.front_default);
+            });
         };
+    });
 
-        // for (i = 0; i < (response.data.types).length; i++) {
-        //     generation[i] = response.data.types[i].type.name;
-        // };
+    g_url = gen_url + id;
+    await axios.get(g_url).then(function (response) {
+        generation = response.data.generation.name;
+    }); 
 
-        for (i = 0; i < (response.data.game_indices).length; i++) {
-            games[i] = response.data.game_indices.name;
-        };
-
-        // console.log(response);
-        console.log('Name: ' + response.data.name + "\nType: " + types);
-        console.log('Weight: ' + weight);
-        console.log('Abilities: ' + abilities)
-        console.log('Strong against: ' + strengths);
-        console.log('Weak against: ' + weaknesses);
-        console.log('Base Attack: ' + base_attack);
-        console.log('Base Defense: ' + base_defense);
-        console.log('Speed: ' + speed);
-        console.log('Evolution Line: ');
-        console.log(evolutions);
-
-
-
-    })
-    poke_object = {name, types, weight, abilities, weaknesses, strengths, base_attack, base_defense, speed, evolutions}
-    return poke_object
+    poke_object = {name, types, weight, abilities, weaknesses, strengths, base_attack, base_defense, speed, evolutions, evo_pic, generation};
+    return poke_object;
 };
 
 const get_all = async () => {
+    
     const p_url = url;
     await axios.get(p_url).then(async function (response) {
         weight = response.data.weight;
