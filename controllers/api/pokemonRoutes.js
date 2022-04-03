71217a99a2d0691203e58776415ad5d8d0f24816
Steve "Homeschool" Seney <sdseney508@gmail.com>
2022-04-03 15:04:53 -0400

    finished pokehelper, can still work on the strengths and weakness; pokeroutes all working, need to work on homeroutes.

--- controllers/api/pokemonRoutes.js
+++ controllers/api/pokemonRoutes.js
@@ -7,38 +7,40 @@ const pokehelper = require('../../utils/pokehelper');
 // route:  /api/pokemon
 //uses an axios fetch call  all the pokemon from the pokedex2 api
 //will need a button to call it from the user page.  See views
-router.GET('/', withAuth, async (req, res) => {
+router.get('/',  async (req, res) => {
     try {
         //make this a get_all
-        const pokemon = await pokehelper.get_all.get(req.params.id);
-        console.log(pokemon);
-        res.status(200).json(newBlog);
+        const pokemons = await pokehelper.get_all();
+        console.log(pokemons);
+        res.status(200).json(pokemons);
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
-//look for info about a certain pokemon
-router.GET('/:id', withAuth, async (req, res) => {
+//look for info about a certain pokemon, have to readd withAuth
+router.get('/:id',  async (req, res) => {
     try {
         //make this get_one
-        const pokemon = await get_one.get(req.params.id);
+        const pokemon = await pokehelper.get_one(req.params.id);
         console.log(pokemon);
-        res.status(200).json(newBlog);
+        res.status(200).json(pokemon);
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
 //tag a favorite
-router.post('/favorite', withAuth, async (req, res) => {
+router.put('/favorite', withAuth, async (req, res) => {
     try {
-        const updatedBlog = await Blog.update({
-            ...req.body,
-            user_id: req.session.user_id,
-        });
+        const updatedFavorite = await UserPokedex.update(
+            { favorite: true },
+            {
+                where: { user_id: req.session.user_id }
+            }
+        );
 
-        res.status(200).json(updatedBlog);
+        res.status(200).json(updatedFavorite);
     } catch (err) {
         res.status(400).json(err);
     }
@@ -47,54 +49,54 @@ router.post('/favorite', withAuth, async (req, res) => {
 //tag a captured
 router.post('/captured', withAuth, async (req, res) => {
     try {
-        const updatedBlog = await Blog.update({
-            ...req.body,
-            user_id: req.session.user_id,
-        });
+        const createdCaptured = await Captured.create(
+            {...req.body, user_id: req.session.user_id,}
+        );
 
-        res.status(200).json(updatedBlog);
+        res.status(200).json(createdCaptured);
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
-//remove favorite
-router.delete('/favorite/:id', withAuth, async (req, res) => {
+//remove a pokemon from a users pokdex.  the where clause uses the pokemon's name and unique id to find the right record
+router.delete('/favorite/:name', withAuth, async (req, res) => {
     try {
-        const blogData = await Blog.destroy({
+        const favoriteData = await UserPokedex.destroy({
             where: {
-                id: req.params.id,
+                namw: req.params.name,
                 user_id: req.session.user_id,
             },
         });
 
-        if (!blogData) {
-            res.status(404).json({ message: 'No blog found to delete with this id.  Please try again' });
+        if (!favoriteData) {
+            res.status(404).json({ message: "No pokemon found to delete from this user's Pokedex.  Please try again." });
             return;
         }
 
-        res.status(200).json(blogData);
+        res.status(200).json(favoriteData);
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
 //remove captured
-router.delete('/captured/:id', withAuth, async (req, res) => {
+//uses the pokemon's name and user id to find the right record to remove from the captured table
+router.delete('/captured/:name', withAuth, async (req, res) => {
     try {
-        const blogData = await Blog.destroy({
+        const capturedData = await Captured.destroy({
             where: {
-                id: req.params.id,
+                name: req.params.name,
                 user_id: req.session.user_id,
             },
         });
 
-        if (!blogData) {
+        if (!capturedData) {
             res.status(404).json({ message: 'No blog found to delete with this id.  Please try again' });
             return;
         }
 
-        res.status(200).json(blogData);
+        res.status(200).json(capturedData);
     } catch (err) {
         res.status(500).json(err);
     }
--- fetchpv2.js
+++ fetchpv2.js
@@ -1,11 +1,12 @@
 Pokemon = require('pokemon.js');
 Pokemon.setLanguage('english');
 const axios = require('axios');
-const { response } = require('express');
+const poketype = require('./utils/poketype');
+const pokehelper = require('./utils/pokehelper');
 
-let poke_id = 15;
-let id = 15;
-const irl = 3;
+let poke_id = 1;
+let id = 1;
+const irl = 1;
 const url = `https://pokeapi.co/api/v2/pokemon/${poke_id}`;
 const all_url = `https://pokeapi.co/api/v2/pokemon/`;
 let gen_url = 'https://pokeapi.co/api/v2/pokemon-species/';
@@ -19,87 +20,110 @@ let pokeobjects = [];
 
 //     }
 // }
+const poke_type = [{ "name": "Normal", "immunes": ["Ghost"], "weaknesses": ["Rock", "Steel"], "strengths": [] },
+{ "name": "Fire", "immunes": [], "weaknesses": ["Fire", "Water", "Rock", "Dragon"], "strengths": ["Grass", "Ice", "Bug", "Steel"] },
+{ "name": "Water", "immunes": [], "weaknesses": ["Water", "Grass", "Dragon"], "strengths": ["Fire", "Ground", "Rock"] },
+{ "name": "Electric", "immunes": ["Ground"], "weaknesses": ["Electric", "Grass", "Dragon"], "strengths": ["Water", "Flying"] },
+{ "name": "Grass", "immunes": [], "weaknesses": ["Fire", "Grass", "Poison", "Flying", "Bug", "Dragon", "Steel"], "strengths": ["Water", "Ground", "Rock"] },
+{ "name": "Ice", "immunes": [], "weaknesses": ["Fire", "Water", "Ice", "Steel"], "strengths": ["Grass", "Ground", "Flying", "Dragon"] },
+{ "name": "Fighting", "immunes": ["Ghost"], "weaknesses": ["Poison", "Flying", "Psychic", "Bug", "Fairy"], "strengths": ["Normal", "Ice", "Rock", "Dark", "Steel"] },
+{ "name": "Poison", "immunes": ["Steel"], "weaknesses": ["Poison", "Ground", "Rock", "Ghost"], "strengths": ["Grass", "Fairy"] },
+{ "name": "Ground", "immunes": ["Flying"], "weaknesses": ["Grass", "Bug"], "strengths": ["Fire", "Electric", "Poison", "Rock", "Steel"] },
+{ "name": "Flying", "immunes": [], "weaknesses": ["Electric", "Rock", "Steel"], "strengths": ["Grass", "Fighting", "Bug"] },
+{ "name": "Psychic", "immunes": ["Dark"], "weaknesses": ["Psychic", "Steel"], "strengths": ["Fighting", "Poison"] },
+{ "name": "Bug", "immunes": [], "weaknesses": ["Fire", "Fighting", "Poison", "Flying", "Ghost", "Steel", "Fairy"], "strengths": ["Grass", "Psychic", "Dark"] },
+{ "name": "Rock", "immunes": [], "weaknesses": ["Fighting", "Ground", "Steel"], "strengths": ["Fire", "Ice", "Flying", "Bug"] },
+{ "name": "Ghost", "immunes": ["Normal"], "weaknesses": ["Dark"], "strengths": ["Psychic", "Ghost"] },
+{ "name": "Dragon", "immunes": ["Fairy"], "weaknesses": ["Steel"], "strengths": ["Dragon"] },
+{ "name": "Dark", "immunes": [], "weaknesses": ["Fighting", "Dark", "Fairy"], "strengths": ["Psychic", "Ghost"] },
+{ "name": "Steel", "immunes": [], "weaknesses": ["Fire", "Water", "Electric", "Steel"], "strengths": ["Ice", "Rock", "Fairy"] },
+{ "name": "Fairy", "immunes": [], "weaknesses": ["Fire", "Poison", "Steel"], "strengths": ["Fighting", "Dragon", "Dark"] }]
 
 const init = async () => {
-    // await axios.get(all_url).then(async function (response) {
+    pokehelper.get_all()
+        .then(console.log);
+    // let name = '';
+    // let evolutions = [];
+    // let generation = [];
+    // let types = [];
+    // let abilities = [];
+    // let strengths = [];
+    // let weaknesses = [];
+    // let weight = '';
+    // let base_hp = '';
+    // let base_attack = '';
+    // let base_defense = '';
+    // let speed = '';
+    // let image_url = '';
+    // let evo_pic = [];
+    // let newurl = all_url + irl;
 
-    //     for (irl = 148; irl < 155; irl++) {
-            let name = '';
-            let evolutions = [];
-            let generation = [];
-            let types = [];
-            let abilities = [];
-            let strengths = [];
-            let weaknesses = [];
-            let weight = '';
-            let base_hp = '';
-            let base_attack = '';
-            let base_defense = '';
-            let speed = '';
-            let image_url = '';
-            let evo_pic = [];
-            
-            let newurl = 'https://pokeapi.co/api/v2/pokemon/' + irl;
-            
-            // g_url = gen_url + irl;
-            // await axios.get(g_url).then(function (response) {
-            //     generation = response.data.generation.name;
-            // });
-
-            await axios.get(newurl).then(async function (response) {
-                name = response.data.name;
-                weight = response.data.weight;
-                base_hp = response.data.stats[0].base_stat;
-                base_attack = response.data.stats[1].base_stat;
-                base_defense = response.data.stats[2].base_stat;
-                speed = response.data.stats[5].base_stat;
-                image_url = response.data.sprites.front_default;
-             
-                types = response.data.types.map(({type}) => type.name);
-                abilities = response.data.abilities.map(({ability}) => ability.name)
-                // for (let i = 0; i < (response.data.types).length; i++) {
-                //     types[i] = response.data.types[i].type.name;
-                // };
-
-                // for (let i = 0; i < (response.data.abilities).length; i++) {
-                //     abilities[i] = response.data.abilities[i].ability.name;
-                // };
-
-                //sequence through the types and create an arrayof types they are strong and weak against
-                for (i = 0; i < (types).length; i++) {
-                    //first strengthes then weaknesses
-                    //get the type of pokemon
-                    let ptype = types[i];
-    
-                    // pass that to the api via a string literal
-                    let type_url = `https://pokeapi.co/api/v2/type/${ptype}`
-    
-                    //make the axios call
-                    await axios.get(type_url).then(function (response) {
-                        // get strengths by looping through the double damage to array
-                        for (let i = 0; i < response.data.damage_relations.double_damage_to.length; i++) {
-                            strengths[i] = response.data.damage_relations.double_damage_to[i].name;
-                            weaknesses[i] = response.data.damage_relations.double_damage_from[i].name;
-                        };
-                    });
-                };
-            });
-
-            await Pokemon.getEvolutionLine(irl).then(async function (response) {
-                for (let ev = 0; ev < response.length; ev++) {
-                    evolutions[ev] = response[ev];
-                    evo_pic.push(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${evolutions[ev]}.png`);
-                };
-            });
-
-            poke_object = { irl, name, types, weight, abilities, weaknesses, strengths, base_attack, base_defense, speed, evolutions, evo_pic, generation };
-            
-            console.log(newurl);
-            // console.log(evo_pic);
-            pokeobjects.push(poke_object);
-        // };
+    // g_url = gen_url + irl;
+    // await axios.get(g_url).then(function (response) {
+    //     generation = response.data.generation.name;
     // });
-    console.log(pokeobjects);
+
+    // await axios.get(newurl).then(async function (response) {
+    //     name = response.data.name;
+    //     weight = response.data.weight;
+    //     base_hp = response.data.stats[0].base_stat;
+    //     base_attack = response.data.stats[1].base_stat;
+    //     base_defense = response.data.stats[2].base_stat;
+    //     speed = response.data.stats[5].base_stat;
+    //     image_url = response.data.sprites.front_default;
+
+    //     types = response.data.types.map(({ type }) => type.name);
+    //     abilities = response.data.abilities.map(({ ability }) => ability.name)
+ 
+    //     //sequence through the types and create an arrayof types they are strong and weak against
+    //     for (i = 0; i < (types).length; i++) {
+    //         // pass that to the api via a string literal
+    //         let type_url = `https://pokeapi.co/api/v2/type/${types[i]}`
+
+    //         //make the axios call
+    //         await axios.get(type_url).then(function (response) {
+    //             // get strengths by looping through the double damage to array
+    //             for (let i = 0; i < response.data.damage_relations.double_damage_to.length; i++) {
+    //                 strengths[i] = response.data.damage_relations.double_damage_to[i].name;
+    //                 weaknesses[i] = response.data.damage_relations.double_damage_from[i].name;
+    //             };
+    //         });
+    //     };
+    //     // for (i = 0; i < (types).length; i++) {
+    //     //     console.log(poketype.strengths(types[i]));
+    //     //     strengths.push(poketype.strengths(types[i]));
+    //     //     weaknesses.push(poketype.weaknesses(types[i]));
+    //     // };
+    //     // for (i = 0; i < (types).length; i++) {
+    //     //     let ptype = types[i];
+    //     //     s_and_w = poke_type.find(function (type) {
+    //     //         if (type.name == ptype) {
+    //     //             return true;
+    //     //         };
+    //     //         return s_and_w;
+    //     //     });
+    //     //     strengths.push(s_and_w.strengths);
+    //     //     console.log(strengths);
+    //     //     // strengths.push(poketype.strengths(types[i]));
+    //     //     // weaknesses.push(poketype.weaknesses(types[i]));
+    //     // };
+    // });
+
+    // await Pokemon.getEvolutionLine(irl).then(async function (response) {
+    //     for (let ev = 0; ev < response.length; ev++) {
+    //         evolutions[ev] = response[ev];
+    //         evo_pic.push(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${evolutions[ev]}.png`);
+    //     };
+    // });
+
+    // poke_object = { irl, name, types, weight, abilities, weaknesses, strengths, base_attack, base_defense, speed, evolutions, evo_pic, generation };
+
+    // console.log(newurl);
+    // // console.log(evo_pic);
+    // pokeobjects.push(poke_object);
+    // // };
+    // // });
+    // console.log(pokeobjects);
 };
 
 init();
