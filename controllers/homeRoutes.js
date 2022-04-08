82336ffbfc32c31d48fd3fbf78b27dc99ede3c08
pcragnol <94771135+pcragnol@users.noreply.github.com>
2022-04-08 18:43:09 -0400

    Started the handlebars for captured Pokémon and changed some file names

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -64,10 +64,10 @@ router.get('/pokemon/:id', async (req, res) => {
   }
 });
 
-router.get('/create_account', (req, res) => {
+router.get('/signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (!req.session.logged_in) {
-    res.render('create_account');
+    res.render('signup');
     return;
   }
   res.redirect('/dashboard');
